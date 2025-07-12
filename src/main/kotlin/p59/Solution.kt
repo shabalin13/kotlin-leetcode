6f3b19c1219dea65ff6281d5ef49fe55cee5769d
Shabalin13 <d.shabal1n12@gmail.com>
2025-07-12 13:18:33 +0300

    Add another solution for problem 59

--- src/main/kotlin/p59/Solution.kt
+++ src/main/kotlin/p59/Solution.kt
@@ -1,6 +1,7 @@
 package p59
 
 class Solution {
+    /*
     fun generateMatrix(n: Int): Array<IntArray> {
         val matrix = Array(n) { IntArray(n) }
 
@@ -20,6 +21,37 @@ class Solution {
         }
 
         if (n % 2 != 0) matrix[n / 2][n / 2] = n * n
+        return matrix
+    }
+     */
+
+    fun generateMatrix(n: Int): Array<IntArray> {
+        val matrix = Array(n) { IntArray(n) }
+
+        var top = 0
+        var bottom = n - 1
+        var left = 0
+        var right = n - 1
+
+        var curNum = 1
+        while (left <= right && top <= bottom) {
+            for (i in left..right) {
+                matrix[top][i] = curNum++
+            }
+            top++
+            for (i in top..bottom) {
+                matrix[i][right] = curNum++
+            }
+            right--
+            for (i in right downTo left) {
+                matrix[bottom][i] = curNum++
+            }
+            bottom--
+            for (i in bottom downTo top) {
+                matrix[i][left] = curNum++
+            }
+            left++
+        }
         return matrix
     }
 }
